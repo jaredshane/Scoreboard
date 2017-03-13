cafffce542d7a4296b5c831c70906896a382cc7b
Jared Fuller <jaredshane@gmail.com>
2017-03-13 16:22:36 -0500

    reorganized some files to get it to work with a new treehouse course

---
+++ app.js
@@ -0,0 +1,17 @@
+//libs
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+//components
+
+import Scoreboard from './components/Scoreboard.js';
+
+//css
+
+import './app.css';
+
+
+
+ReactDOM.render(<Scoreboard />, document.getElementById('container'));
